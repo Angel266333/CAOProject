586af53f0a54819dbd78e3da5e797cb2752dc7f9
Angel Petrov <angelilianov@gmail.com>
2018-10-30 11:01:45 +0100

    Refactor workspace

--- DiodeLoopON_OFF.asm
+++ DiodeLoopON_OFF.asm
@@ -1,4 +1,12 @@
-.include "/home/kup/m2560def.inc"
+;
+; DiodeLoopON_OFF.asm
+;
+; Created: 30/10/2018 09.58.03
+; Author : AI2-PC
+;
+
+
+; Replace with your application code
 ;set directions
 ldi r16, 0b00000001
 out ddrb, r16
@@ -7,17 +15,18 @@ out ddra, r16
 
 ;logic
 start:
-in r16, porta
+; Use 'pin' for input!
+in r16, pina
 cpi r16, 0b00001000
 breq on
 jmp off
 
-on:
+off:
 ldi r17, 0b00000001
 out portb, r17
 jmp start
 
-off:
+on:
 ldi r17, 0b00000000
 out portb, r17
 jmp start
